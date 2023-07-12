637988a22e4bfbb78a33ec5b57a8b3bec1839cc0
Siddeshxworkz <siddeshsiddu9353@gmail.com>
2023-07-12 20:57:50 +0530

    adding assignment file

---
+++ Baloon.java
@@ -0,0 +1,20 @@
+class Baloon {
+
+public static void main (String[] args) {
+
+String colours = "pink";
+float cost = 99.9f;
+String numBaloon = "1 pack";
+String emojiBaloon = "smiley";
+String size = "light";
+
+System.out.println("colours:"+colours);
+System.out.println("cost:"+cost);
+System.out.println("numBaloon:"+numBaloon);
+System.out.println("emojiBaloon:"+emojiBaloon);
+System.out.println("size:"+size);
+
+
+}
+
+}
